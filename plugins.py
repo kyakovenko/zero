b39437a4ef0cea2ba004a7506ba43e4f9114db96
Kirill Yakovenko <yakovenko@crystalnix.com>
2016-06-05 14:37:58 +0600

    Zero can return its network settings.

--- plugins.py
+++ plugins.py
@@ -7,15 +7,17 @@ from slackbot.bot import listen_to
 from sensors.db.session import DBSession
 from sensors.db.models import RoomMetrics
 
+from netifaces import interfaces, ifaddresses, AF_INET, AF_INET6
 
-@respond_to(u'^(hi|hello|привет)', re.IGNORECASE)
+
+@respond_to(u'^(hi|hello)', re.IGNORECASE)
 def hi(message, phrase):
     # react with thumb up emoji
     message.react('+1')
     message.reply("Hello! I'm glad to see you!")
 
 
-@respond_to(u'(^status|how are you\?|как дела\?)', re.IGNORECASE)
+@respond_to(u'(^status$|how are you\?)', re.IGNORECASE)
 def how_are_you(message, phrase):
     message.reply("Everything is fine. I'm working!\nYou can ask me what I can do in order to know more.")
 
@@ -33,18 +35,45 @@ def data(message):
     message.reply("Temperature: {0} *C \nHumidity:     {1} %".format(last_metric.temperature, last_metric.humidity))
 
 
+@respond_to(u'(where are you\?|^detect_place$)', re.IGNORECASE)
+def detect_place(message, phrase):
+    addrs = {}
+    for interface in interfaces():
+        if interface not in ['lo', 'lo0', 'awdl0']:
+            info = ifaddresses(interface)
+            ip4 = info.get(AF_INET, [{}])[0].get('addr', None)
+            ip6 = info.get(AF_INET6, [{}])[0].get('addr', None)
+            if ip4 or ip6:
+                addrs[interface] = [ip4, ip6]
+    if addrs:
+        body = '\n'.join(
+            '    - {0}\n'\
+            '        ipv4: {1}\n'\
+            '        ipv6: {2}'.format(
+                i, a[0], a[1]
+            ) for i, a in addrs.items()
+        )
+        message.reply('My network settings:\n' + body)
+
+
 @respond_to('stat$', re.IGNORECASE)
 @respond_to('stat (.*) (.*)', re.IGNORECASE)
 def stats(message, start_date=None, end_date=None):
    message.reply("Sorry, unfortunatelly, I can't return a statistics.")
 
 
-@respond_to(u'(что ты умеешь делать\?|What can you do\?|help)', re.IGNORECASE)
+@respond_to('test', re.IGNORECASE)
+def test(message):
+    message.reply(str(dir(message)))
+
+
+@respond_to(u'(What can you do\?|^help$)', re.IGNORECASE)
 def help(message, phrase):
     message.reply("""
 Hello, I'm zero. I'm a slack bot and I can execute the following commands:
 
-hi (hello, привет) - I can greet you.
-status (how are you? как дела?) - I can inform you about my status.
+hi (hello) - I can greet you.
+status (how are you?) - I can inform you about my status.
+detect_place (where are you?) - I can say you my network settings using which you can connect to me.
 data - I can return the latest information from sensors.
 """)
